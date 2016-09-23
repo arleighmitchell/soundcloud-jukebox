6470d9aff84dafb7b0d98a04326eb7a08981f6a6
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-23 14:47:13 -0700

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+	
+	SC.initialize({
+	  client_id: '5c10bec7cd919b6f75937bedc71e4a0a'
+	});	
+
+  	$("#search_button").click(function() {
+  		var searchValue = $("#search").val();
+
+  		SC.get('/tracks', {
+  			q: searchValue, license: 'cc-by-sa'
+			}).then(function(tracks) {
+				ids = tracks.map(function(track) {
+					return track.id;
+				});
+
+				console.log(ids);
+  		});
+  	});
+
+  	i = 0;
+
+	$("#play").click(function() {
+		SC.stream('/tracks/' + ids[i]).then(function(player){
+	  		player.play();
+		});
+	});
+
+	$("#pause").click(function() {
+		SC.stream('/tracks/' + ids[i]).then(function(player) {
+			player.pause();
+		});
+		
+	});
+
+	$("#next").click(function() {
+		if (i < ids.length) {
+			SC.stream('/tracks/' + ids[i]).then(function(player){
+	  		player.play();
+			});
+			i = i +1;
+			console.log(i);
+
+		} else {
+			i = 0;
+			console.log(i);
+
+		};
+
+	});
+
+		$("#back").click(function() {
+		if (i < ids.length) {
+			SC.stream('/tracks/' + ids[i]).then(function(player){
+	  		player.play();
+			});
+			i = i - 1;
+			console.log(i);
+
+		} else {
+			i = 0;
+			console.log(i);
+
+		};
+
+	});
+
+});
